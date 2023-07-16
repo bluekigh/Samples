9c75205012d140d2ea45850a64cd9df004ece3c2
bluekigh <bluekigh@gmail.com>
2023-07-16 19:50:58 +0900

    dungeongenerator , ImageLoader in web . Singlton

---
+++ Assets/2.Script/ImageLoadinServer/LoadImage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+[RequireComponent(typeof(RawImage))]
+public class Test : MonoBehaviour
+{
+    
+    public RawImage img;
+    public string imageurl = null;
+    private void Awake()
+    {
+        img = GetComponent<RawImage>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetTexture(img));
+    }
+
+    IEnumerator GetTexture(RawImage img)
+    {
+       
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(imageurl);
+        yield return www.SendWebRequest();
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            img.texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        }
+    }
+}
